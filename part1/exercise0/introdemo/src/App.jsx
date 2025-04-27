075a0d8ddac125327ba9a7431c82d8f392b2ba6c
djrdj <djordjet.pokazivac@gmail.com>
2025-04-27 21:49:35 +0200

    promena 4

--- part1/exercise0/introdemo/src/App.jsx
+++ part1/exercise0/introdemo/src/App.jsx
@@ -1,20 +1,22 @@
 import React from "react"
 
-const Hello = () => {
+const Hello = (props) => {
+  console.log(props)
   return  (
     <div>
-      <p> Hello world </p>
+      <p> Hello world {props.name}, you are {props.age} years old</p>
     </div>
   )
 }
 
 const App = () => {
+  const name = "Peter"
+  const age = 10
   return (
     <div>
       <h1>Greetings</h1>
-      <Hello />
-      <Hello />
-      <Hello />
+      <Hello name='George' age={26+2}/>
+      <Hello name={name} age={age}/>
     </div>
   )
 }
