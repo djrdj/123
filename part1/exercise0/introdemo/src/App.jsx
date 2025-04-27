da5eac5ad6d01182498664dfac49954d72a424a3
djrdj <djordjet.pokazivac@gmail.com>
2025-04-27 21:28:22 +0200

    change 2

--- part1/exercise0/introdemo/src/App.jsx
+++ part1/exercise0/introdemo/src/App.jsx
@@ -1,14 +1,19 @@
+import React from "react"
+
 const App = () => {
   const now = new Date()
   const a=10
   const b=20
   console.log(now, a+b)
-  return (
-    <div>
-      <p>
-        Hello world! It's {now.toString()}
-      </p>
-    </div>  
+  return React.createElement(
+    'div',
+    null,
+    React.createElement(
+      'p',null,'Hello world, it is ', now.toString()
+    ),
+    React.createElement(
+      'p',null,a,' plus ',b,' is ', a+b
+    )
   )
 }
 
