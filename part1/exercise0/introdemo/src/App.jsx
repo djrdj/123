9371a5e7054e19164ee90ac9f5a912db9c0b5b65
djrdj <djordjet.pokazivac@gmail.com>
2025-04-27 17:45:29 +0200

    change1

--- part1/exercise0/introdemo/src/App.jsx
+++ part1/exercise0/introdemo/src/App.jsx
@@ -1,9 +1,12 @@
 const App = () => {
-  console.log('hellooooooooooo from component')
+  const now = new Date()
+  const a=10
+  const b=20
+  console.log(now, a+b)
   return (
     <div>
       <p>
-        Hello world!
+        Hello world! It's {now.toString()}
       </p>
     </div>  
   )
