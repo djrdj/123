0db2f18bec078e163f3ac81c4f7de6992436f22f
djrdj <djordjet.pokazivac@gmail.com>
2025-04-29 11:58:31 +0200

    1.3

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -13,21 +13,21 @@ const Part = ({name, exercises}) => {
   )
 }
 
-const Content = ({part1,part2,part3,exercises1,exercises2,exercises3}) => {
+const Content = ({part1,part2,part3}) => {
   
   return (
     <>
-      <Part name={part1} exercises={exercises1}/>
-      <Part name={part2} exercises={exercises2}/>
-      <Part name={part3} exercises={exercises3}/>
+      <Part name={part1.name} exercises={part1.exercises}/>
+      <Part name={part2.name} exercises={part2.exercises}/>
+      <Part name={part3.name} exercises={part3.exercises}/>
     </>
   )
 }
 
-const Total = ({exercises1,exercises2,exercises3}) => {
+const Total = ({part1,part2,part3}) => {
   return(
     <>
-      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+      <p>Number of exercises {part1.exercises + part2.exercises + part3.exercises}</p>
     </>
   )
   
@@ -36,18 +36,23 @@ const Total = ({exercises1,exercises2,exercises3}) => {
 
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10
+  }
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises: 7
+  }
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14
+  }
   return (
     <div>
       <Header course={course}/>
-      <Content part1={part1} part2={part2} part3={part3} exercises1={exercises1} 
-      exercises2={exercises2} exercises3={exercises3}/>
-      <Total exercises1={exercises1} exercises2={exercises2} exercises3={exercises3}></Total>
+      <Content part1={part1} part2={part2} part3={part3}/>
+      <Total part1={part1} part2={part2} part3={part3}></Total>
     </div>
   )
 }
