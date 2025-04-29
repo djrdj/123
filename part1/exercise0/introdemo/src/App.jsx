f3ca66798f2e4d345f2f4e7a73f765140f77a9ae
djrdj <djordjet.pokazivac@gmail.com>
2025-04-29 11:49:16 +0200

    continue exercising

--- part1/exercise0/introdemo/src/App.jsx
+++ part1/exercise0/introdemo/src/App.jsx
@@ -10,17 +10,26 @@ import React from "react"
 // }
 
 const App = () => {
-  const friends = [
-    { name: "Petar", age: 10},
-    {name: "Jovan", age: 20}
-  ]
+  // const friends = [
+  //   { name: "Petar", age: 10},
+  //   {name: "Jovan", age: 20}
+  // ]
+
+  const t=[1,2,3]
+  const t2=t.concat(4)
+  const [first,second, ...rest]=t2
+  t2.forEach(element => {
+    console.log(element)
+  });
+  console.log("ostatak ",rest)
   return (
     <>
-      <h1>Greetings</h1>
+      {/* <h1>Greetings</h1>
       <p>{friends[0].name} {friends[0].age}</p>
-      <p>{friends[1].name} {friends[1].age}</p>
+      <p>{friends[1].name} {friends[1].age}</p> */}
       {/* <Hello name='George' age={26+2}/>
       <Hello name={name} age={age}/> */}
+      
     </>
   )
 }
