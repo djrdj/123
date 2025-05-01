a61026bead7561feee89a5ebc5536c1f1cd63a2a
djrdj <djordjet.pokazivac@gmail.com>
2025-05-01 11:20:39 +0200

    useState exercise

--- part1/exercise0/introdemo/src/App.jsx
+++ part1/exercise0/introdemo/src/App.jsx
@@ -1,36 +1,16 @@
 import React from "react"
-
-// const Hello = (props) => {
-//   console.log(props)
-//   return  (
-//     <div>
-//       <p> Hello world {props.name}, you are {props.age} years old</p>
-//     </div>
-//   )
-// }
+import { useState } from 'react'
 
 const App = () => {
-  // const friends = [
-  //   { name: "Petar", age: 10},
-  //   {name: "Jovan", age: 20}
-  // ]
+  const [ counter, setCounter ] = useState(0)
 
-  const t=[1,2,3]
-  const t2=t.concat(4)
-  const [first,second, ...rest]=t2
-  t2.forEach(element => {
-    console.log(element)
-  });
-  console.log("ostatak ",rest)
+  setTimeout(
+    () => setCounter(counter + 1),
+    1000
+  )
+
   return (
-    <>
-      {/* <h1>Greetings</h1>
-      <p>{friends[0].name} {friends[0].age}</p>
-      <p>{friends[1].name} {friends[1].age}</p> */}
-      {/* <Hello name='George' age={26+2}/>
-      <Hello name={name} age={age}/> */}
-      
-    </>
+    <div>{counter}</div>
   )
 }
 
