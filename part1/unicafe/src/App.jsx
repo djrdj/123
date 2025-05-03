76a8cf8431e5284501dd622a617189956d1e5f05
djrdj <djordjet.pokazivac@gmail.com>
2025-05-03 10:30:52 +0200

    1.10

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 
 const Statistics = ({numbers}) => {
   if (numbers[0]==0 && numbers[1]==0 && numbers[2] == 0)
-    return  <div> <h1>statistics</h1> No feedback given</div>
+    return  <div> No feedback given</div>
   let all = 0, average = 0, positive = 0
   numbers.forEach(element => {
     all+=element
@@ -11,16 +11,18 @@ const Statistics = ({numbers}) => {
   positive=numbers[0]/all*100
   return (
     <div>
-      <h1>statistics</h1>
-      <p>good {numbers[0]}</p>
-      <p>neutral {numbers[1]}</p>
-      <p>bad {numbers[2]}</p>
+      
+      <StatisticLine text="good" value ={numbers[0]} />
+      <StatisticLine text="neutral" value ={numbers[1]} />
+      <StatisticLine text="bad" value ={numbers[2]} />
       <p>all {all}</p>
       <p>average {average}</p>
       <p>positive {positive}%</p>
     </div>
   )
 }
+
+const StatisticLine = ({text,value}) => <div> <p>{text} {value}</p> </div>
 
 const App = () => {
   // save clicks of each button to its own state
@@ -34,6 +36,7 @@ const App = () => {
       <button onClick={()=>setGood(good+1)}>good</button>
       <button onClick={()=>setNeutral(neutral+1)}>neutral</button>
       <button onClick={()=>setBad(bad+1)}>bad</button>
+      <h1>statistics</h1>
       <Statistics numbers={[good,neutral,bad]} />
     </div>
   )
