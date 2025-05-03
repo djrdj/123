d5aa933ef42e32d10951d027460b59effe437366
djrdj <djordjet.pokazivac@gmail.com>
2025-05-03 10:10:54 +0200

    1.8 & 1.9

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,14 +1,20 @@
 import { useState } from 'react'
 
 const Statistics = ({numbers}) => {
+  if (numbers[0]==0 && numbers[1]==0 && numbers[2] == 0)
+    return  <div> <h1>statistics</h1> No feedback given</div>
   let all = 0, average = 0, positive = 0
   numbers.forEach(element => {
     all+=element
   })
-  average=all/numbers.length
+  average=numbers[0]-numbers[2]
   positive=numbers[0]/all*100
   return (
     <div>
+      <h1>statistics</h1>
+      <p>good {numbers[0]}</p>
+      <p>neutral {numbers[1]}</p>
+      <p>bad {numbers[2]}</p>
       <p>all {all}</p>
       <p>average {average}</p>
       <p>positive {positive}%</p>
@@ -28,10 +34,6 @@ const App = () => {
       <button onClick={()=>setGood(good+1)}>good</button>
       <button onClick={()=>setNeutral(neutral+1)}>neutral</button>
       <button onClick={()=>setBad(bad+1)}>bad</button>
-      <h1>statistics</h1>
-      <p>good {good}</p>
-      <p>neutral {neutral}</p>
-      <p>bad {bad}</p>
       <Statistics numbers={[good,neutral,bad]} />
     </div>
   )
