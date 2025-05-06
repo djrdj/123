fa97b9d1d36c65ade5d7a552e70970b22c0f1441
djrdj <djordjet.pokazivac@gmail.com>
2025-05-06 11:37:35 +0200

    2.3

--- part2/courseinfo/src/components/Sum.jsx
+++ part2/courseinfo/src/components/Sum.jsx
@@ -1,12 +1,12 @@
-const Sum = ({parts}) => {
-    let sum=0
-    parts.map(part=>sum+=part.exercises)
+const Sum = ({ parts }) => {
+    const total = parts.reduce((sum, part) => sum + part.exercises, 0)
+
     return (
         <div>
-            <b>total of {sum} exercises</b>
+            <b>total of {total} exercises</b>
         </div>
     )
-    
+
 }
 
 export default Sum
